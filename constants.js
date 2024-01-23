e0ae0e20b28ed55c0da039588760a1d1a65a25ec
Bernardo Guerreiro <bernardoguerr@gmail.com>
2024-01-23 16:47:32 +0000

    refactor: update constants.js

--- constants.js
+++ constants.js
@@ -1,13 +1,9 @@
-const SERVICE_NAME = "movies-service";
+const SERVICE_NAME = "movie-service";
 const REGION = process.env.DEPLOYMENT_REGION || "eu-west-2";
 
-if(!process.env.DDB_TABLE_NAME || (!process.env.SERVICE_NAME && !process.env.ENVIRONMENT)) {
-    throw new Error('TABLE_NAME environment variable not set');
-}
-
-const TABLE_NAME = process.env.DDB_TABLE_NAME || `${SERVICE_NAME}-${ENVIRONMENT}`;
-
 module.exports = {
+    SERVICE_NAME: 'movie-service',
     REGION,
-    TABLE_NAME,
+    TABLE_NAME: process.env.DDB_TABLE_NAME || `${SERVICE_NAME}-${ENVIRONMENT}`,
+//     TABLE_NAME,
 }
